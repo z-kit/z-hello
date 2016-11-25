71bf2e0545ede64e43443bce1d13d17203b2af6c
Matheus Kautzmann <kautzmann5@gmail.com>
2016-11-25 19:37:28 -0200

    Add demo for checking and testing

---
+++ demo/render.js
@@ -0,0 +1,16 @@
+import { h, render as pRender } from 'preact';
+import React from 'react';
+import { render as rRender } from 'react-dom';
+import component from '../dist/zcomponent.es.js';
+
+const PreactComponent = component(h);
+const ReactComponent = component(React.createElement);
+
+const preactContainer = document.querySelector('.preact-container');
+const reactContainer = document.querySelector('.react-container');
+
+// Rendering with preact
+pRender(h(PreactComponent, {}, 'Hello World!'), preactContainer);
+
+// Rendering with React
+rRender(React.createElement(ReactComponent, {}, 'Hello World!'), reactContainer);
