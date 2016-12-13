1ec5ef3032d18c9efce83807084f195924a872aa
Matheus Kautzmann <kautzmann5@gmail.com>
2016-12-13 17:25:47 -0200

    Add more browsers as targets

--- test/regression/setup/gemini.conf.js
+++ test/regression/setup/gemini.conf.js
@@ -1,5 +1,6 @@
 const projectRoot = require('./projectRoot');
 
+// Chrome is default, must be present in the dev environment
 const browsers = {
   chrome: {
     desiredCapabilities: {
@@ -7,6 +8,38 @@ const browsers = {
     },
   },
 };
+
+if (process.env.FIREFOX) {
+  browsers.firefox = {
+    desiredCapabilities: {
+      browserName: 'firefox',
+    },
+  };
+}
+
+if (process.env.SAFARI) {
+  browsers.safari = {
+    desiredCapabilities: {
+      browserName: 'safari',
+    },
+  };
+}
+
+if (process.env.IE) {
+  browsers.ie = {
+    desiredCapabilities: {
+      browserName: 'internet explorer',
+    },
+  };
+}
+
+if (process.env.MS_EDGE) {
+  browsers.msedge = {
+    desiredCapabilities: {
+      browserName: 'MicrosoftEdge',
+    },
+  };
+}
 
 module.exports = {
   system: { projectRoot },
