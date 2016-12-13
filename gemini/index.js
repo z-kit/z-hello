2dfc6d71438f900871c6224a2a04b78341794923
Matheus Kautzmann <kautzmann5@gmail.com>
2016-12-13 15:51:22 -0200

    Add stateless functional component and web component stories

--- gemini/index.js
+++ gemini/index.js
@@ -1,5 +1,17 @@
 gemini.suite('CSS component', (suite) => {
-  suite.setUrl('/')
+  suite.setUrl('/?selectedKind=CSS%20component&selectedStory=default')
+    .setCaptureElements('#storybook-preview-iframe')
+    .capture('plain');
+});
+
+gemini.suite('Stateless functional component', (suite) => {
+  suite.setUrl('/?selectedKind=Stateless%20functional%20component&selectedStory=default')
+    .setCaptureElements('#storybook-preview-iframe')
+    .capture('plain');
+});
+
+gemini.suite('Web Component', (suite) => {
+  suite.setUrl('/?selectedKind=Web%20component&selectedStory=default')
     .setCaptureElements('#storybook-preview-iframe')
     .capture('plain');
 });
