ac6085dd0f8c4b05d2dd6183da2862e93ebf3762
Matheus Kautzmann <kautzmann5@gmail.com>
2017-02-06 09:50:18 -0200

    Use storybook as test server

--- test/regression/suite/css.js
+++ test/regression/suite/css.js
@@ -1,7 +1,7 @@
 module.exports = {
   'Plain render': (browser) => {
     browser
-      .url('http://localhost:3000/iframe.html?selectedKind=CSS%20component&selectedStory=default')
+      .url('http://localhost:6006/iframe.html?selectedKind=CSS%20component&selectedStory=default')
       .waitForElementPresent('.z-hello', 1000)
       .assert.containsText('.z-hello__title', 'Hello!')
       .end();
--- test/regression/suite/webcomponent.js
+++ test/regression/suite/webcomponent.js
@@ -2,7 +2,7 @@ module.exports = {
   '@tags': ['webcomponent'],
   'Plain render': (browser) => {
     browser
-      .url('http://localhost:3000/iframe.html?selectedKind=Web%20component&selectedStory=default')
+      .url('http://localhost:6006/iframe.html?selectedKind=Web%20component&selectedStory=default')
       .waitForElementPresent('body', 1000)
       .assert.containsText('z-hello', 'Hello!')
       .end();
