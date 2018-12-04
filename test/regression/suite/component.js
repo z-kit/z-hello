7d5caef03cd648d56a45c0a3dce4c9f0fb2aa7f1
Matheus Kautzmann <mkvchain@gmail.com>
2018-12-04 14:54:48 -0200

    Update regression tests

--- test/regression/suite/component.js
+++ test/regression/suite/component.js
@@ -1,14 +1,14 @@
-import { test } from 'ava';
+import test from 'ava';
 import Nightmare from 'nightmare';
 
-test('Stateless functional component', (t) => {
+test('Stateless functional component', async (t) => {
   t.plan(1);
   const msg = 'should render the Hello! text';
   const expected = 'Hello!';
-  return Nightmare()
+  const actual = await Nightmare()
     .goto('http://localhost:6006/iframe.html?selectedKind=Stateless%20functional%20component&selectedStory=default')
     .wait('.z-hello')
     .evaluate(() => document.querySelector('.z-hello__title').textContent)
-    .end()
-    .then(actual => t.deepEqual(expected, actual, msg));
+    .end();
+  return t.deepEqual(expected, actual, msg);
 });
--- test/regression/suite/webcomponent.js
+++ test/regression/suite/webcomponent.js
@@ -1,14 +1,14 @@
-import { test } from 'ava';
+import test from 'ava';
 import Nightmare from 'nightmare';
 
-test('Web component', (t) => {
+test('Web component', async (t) => {
   t.plan(1);
   const msg = 'should render the Hello! text';
   const expected = 'Hello!';
-  return Nightmare()
+  const actual = await Nightmare()
     .goto('http://localhost:6006/iframe.html?selectedKind=Web%20component&selectedStory=default')
     .wait('z-hello')
     .evaluate(() => document.querySelector('z-hello').textContent)
-    .end()
-    .then(actual => t.deepEqual(expected, actual, msg));
+    .end();
+  return t.deepEqual(expected, actual, msg);
 });
