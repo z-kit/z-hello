98b393d4df87b4a0f79d42565d6573ce55422b6b
Matheus Kautzmann <mkvchain@gmail.com>
2018-12-04 15:11:17 -0200

    Switch from jsx-test-helpers to enzyme

--- test/unit/suite/index.js
+++ test/unit/suite/index.js
@@ -1,23 +1,26 @@
 import test from 'ava';
 import React from 'react';
-import { renderJSX, JSX } from 'jsx-test-helpers';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, configure } from 'enzyme';
 import { component } from '../../../src/component';
+
+configure({ adapter: new Adapter() });
 
 const Component = component(React.createElement);
 
 test('JSX component', (t) => {
   const msg = 'should render';
-  const actual = renderJSX(
+  const actual = shallow(
     <Component>
       Hello!
     </Component>,
-  );
-  const expected = JSX(
+  ).html();
+  const expected = shallow(
     <div className="z-hello">
       <h1 className="z-hello__title">
         Hello!
       </h1>
     </div>,
-  );
+  ).html();
   return t.deepEqual(actual, expected, msg);
 });
